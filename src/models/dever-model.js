bd77b3839007a6d9de8a94e794f438eee37152e7
LeonardoCorso16 <65674863+LeonardoCorso16@users.noreply.github.com>
2021-04-20 15:59:39 -0300

    Delete src/models directory

--- src/models/dever-model.js
+++
@@ -1,9 +0,0 @@
-class DeverModel {
-    constructor(nome, status, email) {
-        this.id = nome;
-        this.status = status;
-        this.email = email;
-    }
-}
-
-module.exports = DeverModel;
